579ad9ef78738ee6ff34e6c01a4fc791ac9f3f9c
Sylvain T <sylvaintagnabou@gmail.com>
2023-04-29 00:07:33 +0000

    start on server side

--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -1,6 +1,5 @@
 const secp = require('ethereum-cryptography/secp256k1')
 const { toHex } = require('ethereum-cryptography/utils')
-const { getRandomBytesSync } = require("ethereum-cryptography/random");
 
 const privateKey = secp.secp256k1.utils.randomPrivateKey()
 
